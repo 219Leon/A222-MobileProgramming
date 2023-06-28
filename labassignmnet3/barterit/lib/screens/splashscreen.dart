cdfda49b5a8d5f364f4a4c6422e72730e9c639bc
219Leon <ewe.leon@yahoo.com.my>
2023-06-28 13:24:44 +0800

    major modifications

---
+++ labassignmnet3/barterit/lib/screens/splashscreen.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:barterit/screens/mainscreen.dart';
+import '../model/user.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BarterIt',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+ super.initState();
+    User user = User(
+        id: "0",
+        email: "unknown",
+        name: "unregistered",
+        address: "na",
+        phone: "0123456789",
+        regdate: "0",
+        );
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (content) => MainScreen(user: user, selectedIndex: 0,))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/splash_screen.png'),
+                  fit: BoxFit.cover)),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text("BarterIt",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.black)),
+              CircularProgressIndicator(),
+              Text(
+                "Version 1.0.0",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
