e10ac7f72578e5650fb0889aade8a2986b2353c9
219Leon <ewe.leon@yahoo.com.my>
2023-07-19 15:10:27 +0800

    minor changes

--- project/barterit/lib/screens/shared/searchscreen.dart
+++ project/barterit/lib/screens/shared/searchscreen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:barterit/screens/mainscreen.dart';
-import 'package:barterit/screens/trader/traderitemdetails.dart';
+import 'package:barterit/screens/user/useritemdetails.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
